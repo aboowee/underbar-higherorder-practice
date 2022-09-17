5d6d8dc019cbe2cc824e0de78508ee4d96fb9b3c
Anthony Bui <anthonyb50@gmail.com>
2022-09-17 16:41:32 -0400

    Implemented map, pluck, reduce functions

--- src/underbar.js
+++ src/underbar.js
@@ -85,60 +85,40 @@
     return uniqueValues;
   };
 
-
-  // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+
+    var iteratedResults = [];
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iteratedResults.push(iterator(collection[i], i, collection));
+      }
+    } else {
+      for (var key in collection) {
+        iteratedResults.push(iterator(collection[key], key, collection));
+      }
+    }
+    return iteratedResults;
   };
 
-  /*
-   * TIP: map is really handy when you want to transform an array of
-   * values into a new array of values. _.pluck() is solved for you
-   * as an example of this.
-   */
-
-  // Takes an array of objects and returns and array of the values of
-  // a certain property in it. E.g. take an array of people and return
-  // an array of just their ages
   _.pluck = function(collection, key) {
-    // TIP: map is really handy when you want to transform an array of
-    // values into a new array of values. _.pluck() is solved for you
-    // as an example of this.
     return _.map(collection, function(item) {
       return item[key];
     });
   };
 
-  // Reduces an array or object to a single value by repetitively calling
-  // iterator(accumulator, item) for each item. accumulator should be
-  // the return value of the previous iterator call.
-  //
-  // You can pass in a starting value for the accumulator as the third argument
-  // to reduce. If no starting value is passed, the first element is used as
-  // the accumulator, and is never passed to the iterator. In other words, in
-  // the case where a starting value is not passed, the iterator is not invoked
-  // until the second element, with the first element as its second argument.
-  //
-  // Since JavaScript object properties are not stored in any particular order,
-  // we cannot reliably anticpate what property will be accessed first during
-  // property iteration. Given this, it is not necessary for your solution
-  // to be able to handle the case of an object being passed in with no
-  // initial accumulator.
-  //
-  // Example:
-  //   var numbers = [1,2,3];
-  //   var sum = _.reduce(numbers, function(total, number){
-  //     return total + number;
-  //   }, 0); // should be 6
-  //
-  //   var identity = _.reduce([5], function(total, number){
-  //     return total + number * number;
-  //   }); // should be 5, regardless of the iterator function passed in
-  //          No accumulator is given so the first element is used.
+
   _.reduce = function(collection, iterator, accumulator) {
-    // TIP: To support both arrays and objects, try re-using each() here
+    if (accumulator === undefined) {
+      accumulator = collection[0];
+      _.each(collection.slice(1), function (item) {
+        accumulator = iterator(accumulator, item);
+      });
+    } else {
+      _.each(collection, function (item) {
+        accumulator = iterator(accumulator, item);
+      });
+    }
+    return accumulator;
   };
 
 
